b1c01dad44d0f26c467643f003329b7314ca7a63
annaquaglieri16 <quaglieri.q@wehi.edu.au>
2017-12-04 09:48:21 +1100

    updates one year of R-Ladies

--- 2017-10-25_OneYearOfRLadiesMelbourne/test_cronjob.R
+++
@@ -1,8 +0,0 @@
-# test cronjob
-
-dir <- "/home/users/allstaff/quaglieri.a/BioCAsia"
-x <- rnorm(100)
-time <- Sys.Date()
-write.table(x,file.path(dir,paste0("xnorm_",time,".txt")),col.names = FALSE)
-
-# */5 * * * * Rscript /home/users/allstaff/quaglieri.a/BioCAsia/test_cronjob.R
